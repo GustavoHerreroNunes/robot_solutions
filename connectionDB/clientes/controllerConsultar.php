fae49d145d1b595e31082485f9041297a8974ad1
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-09-08 11:42:43 -0300

    Criando classe de análise e apresentação dos registros

---
+++ connectionDB/clientes/controllerConsultar.php
@@ -0,0 +1,61 @@
+<?php
+    require_once("clientes.php");
+
+    class controllerConsultar{
+
+        //Instância
+        private $clientes;
+
+        //Construtor da classe
+        public function __construct(){
+            $this->clientes = new Clientes();
+            $this->executar();
+        }
+
+        //Método que executa a consulta dos registros, verifica se algum foi encontrado e em caso afirmativo os apresenta na página
+        public function executar(){
+            $registries_found = false;//Indica se os registros foram encontrados
+
+            $db_registries = $this->clientes->consultar();
+
+            //Se a variável "db_registries" tiver recebido uma mensagem de erro ao invés dos registros
+            if(is_string($db_registries)){
+
+                echo "<script>alert('".$db_registries."');</script>";
+
+            }else{
+
+                foreach($db_registries as $registry){
+                    $registries_found = true;
+                    ?>
+                    
+                    <tr>
+                        <td><?php echo $registry[1] ?></td>
+                        <td><?php echo $registry[2] ?></td>
+                        <td><?php echo $registry[3] ?></td>
+                        <td><?php echo $registry[4] ?></td>
+                        <td><?php echo $registry[5] ?></td>
+                        <td><a href="./editar.html"><img src="./img/editar.png" width="25" height="25" alt="Editar Registro"></a></td>
+                        <td><a href="./excluir.html"><img src="./img/excluir.png" width="25" height="25" alt="Excluir Registro"></a></td>
+                    </tr>
+                    
+                    <?php
+                }
+
+            }
+
+            //Nenhum registro foi encontrado
+            if(!$registries_found){
+                ?>
+                
+                <tr>
+                    <td colspan=6>Nenhum registro foi encontrado</td>
+                </tr>
+                
+                <?php
+            }
+        }
+    }
+
+    new controllerConsultar();
+?>
